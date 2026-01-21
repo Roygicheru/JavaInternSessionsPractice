25c119dae63f25da3db2be88dafac0ad11ed37f2
roygicheru <gicheru.k.roy@gmail.com>
2026-01-21 08:50:43 +0300

    Updated my code

--- src/main/java/io/github/roygicheru/Main.java
+++ src/main/java/io/github/roygicheru/Main.java
@@ -3,7 +3,7 @@ package io.github.roygicheru;
 import io.github.roygicheru.OOP.OOP1;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main() {
         OOP1.practiceTask();
     }
 }
