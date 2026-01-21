6cb0c16115c71a09def67f5c31a45e0354c0d328
roygicheru <gicheru.k.roy@gmail.com>
2026-01-21 08:21:56 +0300

    Initial commit: Set up Java 25 Maven project for internship practice

---
+++ src/main/java/io/github/roygicheru/Main.java
@@ -0,0 +1,12 @@
+package io.github.roygicheru;
+
+public class Main {
+    static void main() {
+        System.out.println("hello");
+//        IO.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            IO.println("i = " + i);
+        }
+    }
+}
